d889371969a3aa33c19548642307e261fc1621b9
Omar <oser520@gmail.com>
2023-06-11 11:08:42 -0400

    Fix unit tests for magics.

--- test/magics_test.cc
+++ test/magics_test.cc
@@ -6,25 +6,14 @@
 #include "gtest/gtest.h"
 #include "pre_computed_magics.h"
 #include "square.h"
+#include "utils.h"
 
 using namespace blunder;
 
 using ::testing::SizeIs;
 
-// TOOD: put this in utilities for testing.
-MATCHER_P(EqualToSq, squares, "has the following squares set: "
-    + ToListStr(squares))
-{
-  auto bb = ToBitBoard(squares);
-  if (bb == arg) return true;
-
-  auto sq_set = ToSetOfSq(arg);
-  *result_listener << ToListStr(sq_set);
-
-  return false;
-}
-
 constinit std::unique_ptr<MagicAttacks> bmagics = nullptr;
+constinit std::unique_ptr<MagicAttacks> rmagics = nullptr;
 
 class BishopMagicsTest : public testing::Test {
 protected:
@@ -44,9 +33,38 @@ TEST_F(BishopMagicsTest, Contains64Magics)
   EXPECT_THAT(bmagics->GetMagics(), SizeIs(64));
 }
 
-TEST_F(BishopMagicsTest, NoMovesWhenBlocked)
+TEST_F(BishopMagicsTest, WhenBlockedImmediatelyOnAllSides)
 {
-  auto bishop = ToBitBoard({Sq::e4});
+  auto square = ToUint(Sq::e4);
   auto blockers = ToBitBoard({Sq::d3, Sq::d5, Sq::f5, Sq::f3});
-  EXPECT_THAT(bmagics->GetAttacks(bishop, blockers), EqualToSq(SqList{}));
+  EXPECT_THAT(
+      bmagics->GetAttacks(square, blockers),
+      EqualToSq(SqList{Sq::d3, Sq::f3, Sq::d5, Sq::f5}));
+}
+
+class RookMagicsTest : public testing::Test {
+protected:
+  void
+  SetUp() override
+  {
+    if (not rmagics) {
+      auto result = InitFromRookMagics(kRookMagics);
+      ASSERT_TRUE(result) << "Unable to initialize bishop magics.";
+      rmagics = std::make_unique<MagicAttacks>(std::move(*result));
+    }
+  }
+};
+
+TEST_F(RookMagicsTest, Contains64Magics)
+{
+  EXPECT_THAT(bmagics->GetMagics(), SizeIs(64));
+}
+
+TEST_F(RookMagicsTest, WhenBlockedImmediatelyOnAllSides)
+{
+  auto square = ToUint(Sq::e4);
+  auto blockers = ToBitBoard({Sq::e3, Sq::d4, Sq::f4, Sq::e5});
+  EXPECT_THAT(
+      rmagics->GetAttacks(square, blockers),
+      EqualToSq(SqList{Sq::e3, Sq::d4, Sq::f4, Sq::e5}));
 }
