ab9d1c66a40b001a2bd4d96e5370d55e87b0aabb
Omar <oser520@gmail.com>
2023-06-11 11:00:45 -0400

    Don't include edge squares when computing bishop and rook masks.

--- src/bitboard.cc
+++ src/bitboard.cc
@@ -15,28 +15,28 @@ GetRookAttacks(std::uint32_t sq, BitBoard blocking) noexcept
   const int fl = sq % 8;
 
   // Bitscan upwards towards 8th rank.
-  for (int r = rk+1; r <= 7; r++) {
+  for (int r = rk+1; r < 7; ++r) {
     const BitBoard bit = 1ull << (r*8 + fl);
     attacks |= bit;
     if (bit & blocking) break;
   }
 
   // Bitscan downwards towards 1st rank.
-  for (int r = rk-1; r >= 0; r--) {
+  for (int r = rk-1; r > 0; --r) {
     const BitBoard bit = 1ull << (r*8 + fl);
     attacks |= bit;
     if (bit & blocking) break;
   }
 
   // Bitscan east towards the H file.
-  for (int f = fl+1; f <= 7; f++) {
+  for (int f = fl+1; f < 7; ++f) {
     const BitBoard bit = 1ull << (rk*8 + f);
     attacks |= bit;
     if (bit & blocking) break;
   }
 
   // Bitscan west towards the A file.
-  for (int f = fl-1; f >= 0; --f) {
+  for (int f = fl-1; f > 0; --f) {
     const BitBoard bit = 1ull << (rk*8 + f);
     attacks |= bit;
     if (bit & blocking) break;
@@ -55,29 +55,29 @@ GetBishopAttacks(std::uint32_t sq, BitBoard blocking) noexcept
   const int fl = sq % 8;
 
   // Bitscan northeast.
-  for (int r = rk+1, f = fl+1; r <= 7 and f <= 7; ++r, ++f) {
-    const BitBoard bit = 1ull << (f + r*8);
+  for (int r = rk+1, f = fl+1; r < 7 and f < 7; ++r, ++f) {
+    const BitBoard bit = 1ull << (r*8 + f);
     attacks |= bit;
     if (bit & blocking) break;
   }
 
   // Bitscan northwest.
-  for (int r = rk+1, f = fl-1; r <= 7 and f >= 0; ++r, --f) {
-    const BitBoard bit = 1ull << (f + r*8);
+  for (int r = rk+1, f = fl-1; r < 7 and f > 0; ++r, --f) {
+    const BitBoard bit = 1ull << (r*8 + f);
     attacks |= bit;
     if (bit & blocking) break;
   }
 
   // Bitscan southeast.
-  for (int r = rk-1, f = fl+1; r >= 0 and f <= 7; --r, ++f) {
-    const BitBoard bit = 1ull << (f + r*8);
+  for (int r = rk-1, f = fl+1; r > 0 and f < 7; --r, ++f) {
+    const BitBoard bit = 1ull << (r*8 + f);
     attacks |= bit;
     if (bit & blocking) break;
   }
 
   // Bitscan southwest.
-  for (int r = rk-1, f = fl-1; r >= 0 and f >= 0; --r, --f) {
-    const BitBoard bit = 1ull << (f + r*8);
+  for (int r = rk-1, f = fl-1; r > 0 and f > 0; --r, --f) {
+    const BitBoard bit = 1ull << (r*8 + f);
     attacks |= bit;
     if (bit & blocking) break;
   }
