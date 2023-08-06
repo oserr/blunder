4c39354a18dceb1622262db0be8e0269f257a9a0
Omar <oser520@gmail.com>
2023-08-05 21:34:11 -0400

    Begin fleshing out Mcts, a class for monte carlo tree search.

---
+++ src/mcts.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+#include <utility>
+
+#include "evaluator.h"
+#include "search.h"
+
+namespace blunder {
+
+// Monte Carlo Tree Search.
+class Mcts : public Search {
+public:
+  Mcts(std::shared_ptr<Evaluator> evaluator, unsigned simulations)
+    : evaluator(std::move(evaluator)),
+      simuls(simulations) {}
+
+  SearchResult
+  run(const Board& board) const override
+  { return SearchResult(); }
+
+private:
+  std::shared_ptr<Evaluator> evaluator;
+  unsigned simuls;
+};
+
+} // namespace blunder
