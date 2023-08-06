23a0d3c30f5abe15dc7ffba6e539e1eb6b30a64a
Omar <oser520@gmail.com>
2023-08-05 21:33:18 -0400

    Create Evaluator: an interface for evaluating chess positions.

---
+++ src/evaluator.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include "board.h"
+
+namespace blunder {
+
+// Represents the evaluation result of a given chess position.
+struct Prediction {
+  // Vector of move probabilities.
+  std::vector<std::pair<Move, float>> move_probs;
+
+  // A value between [-1, 1] to represent likelihood of winning, drawing, or
+  // losing.
+  float value;
+};
+
+// An abstract interface for evaluating chess positions.
+class Evaluator {
+public:
+  virtual
+  ~Evaluator() = default;
+
+  Prediction
+  predict(const Board& board) const = 0;
+};
+
+} // namespace blunder
