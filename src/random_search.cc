778bb869b85d66f17a6b9967e4626ab83f6cf2be
Omar <oser520@gmail.com>
2023-08-15 23:02:05 -0400

    Create RandomSearch so I can move on to other parts of engine.
    With RandomSearch, I can begin to work on other parts of the engine,
    including building some sort of UI to be able to play games against the
    engine.

---
+++ src/random_search.cc
@@ -0,0 +1,49 @@
+#include "random_search.h"
+
+#include <random>
+#include <stdexcept>
+#include <vector>
+
+namespace blunder {
+
+SearchResult
+RandomSearch::run(const BoardPath& board_path) const
+{
+  auto root = board_path.root();
+  if (not root)
+    throw std::logic_error("BoardPath should have a root.");
+
+  const auto& board = root->get();
+  if (board.is_terminal())
+    throw std::invalid_argument("Board is in a terminal state.");
+
+  auto children = board.next();
+  if (children.empty())
+    throw std::length_error("children should be non-empty.");
+
+  std::vector<MoveProb> moves;
+  moves.reserve(children.size());
+  for (unsigned i = 0; i < children.size(); ++i)
+    moves[i].board = std::move(children[i]);
+
+  std::uniform_int_distribution<unsigned> rand_gen(0, moves.size()-1);
+  for (unsigned i = 0; i < 100; ++i) {
+    auto index = rand_gen(rand_fn);
+    ++moves[index].visits;
+  }
+
+  for (auto& mv : moves)
+    mv.prior = static_cast<float>(mv.visits) / 100.0;
+
+  auto iter = std::max_element(moves.begin(), moves.end());
+
+  SearchResult result {
+    .best = *iter,
+    .moves = std::move(moves),
+    .value = rand_value_fn(rand_fn)
+  };
+
+  return result;
+}
+
+} // namespace blunder
---
+++ src/random_search.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <random>
+
+#include "search.h"
+#include "board_path.h"
+
+namespace blunder {
+
+// Chooses moves randomly. Serves two purposes:
+// 1. Allows me to develop other interface components more quickly because it
+//    will allow me to see how this hooks up with other components, and I can
+//    even work on setting up some sort of UI to play gainst the engine.
+// 2. Eventually, when I start training the engine for real, I can also
+//    measure the peformance of the trained engine against the random engine.
+class RandomSearch : public Search {
+public:
+  // Initialzies RandomSearch with a seed for the random number generator.
+  explicit
+  RandomSearch(unsigned seed)
+    : rand_fn(seed),
+      rand_value_fn(-1.0, 1.0) {}
+
+  SearchResult
+  run(const BoardPath& board_path) const override;
+
+private:
+  mutable std::mt19937 rand_fn;
+  mutable std::uniform_real_distribution<float> rand_value_fn;
+};
+
+} // namespace blunder
