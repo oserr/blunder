ac124fb06669b6f1c5df46da924cce117656108d
Omar <oser520@gmail.com>
2023-08-20 17:32:00 -0400

    Use BoardPath::rev() to create the BoardPath.

--- src/random_player.h
+++ src/random_player.h
@@ -5,6 +5,7 @@
 #include <utility>
 
 #include "board.h"
+#include "board_path.h"
 #include "random_search.h"
 
 namespace blunder {
@@ -18,15 +19,8 @@ public:
   Board
   make_move(std::span<const Board> boards) override
   {
-    // TODO: create a utility to create a BoardPath from a container.
-    BoardPath board_path;
-    auto iter = boards.rbegin();
-
-    while (not board_path.is_full() and iter != boards.end())
-      board_path.push(*iter++)
-
+    auto board_path = BoardPath::rev(boards);
     auto result = rand_search.run(board_path);
-
     return std::move(result.best.board);
   }
 
