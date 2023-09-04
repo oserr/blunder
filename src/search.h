d8c3ae375c00c0b7c169aae461a157758ddbbb4d
Omar <oser520@gmail.com>
2023-09-03 21:33:41 -0400

    Init values to zero and add TODO to include more stuff in SearchResults.

--- src/search.h
+++ src/search.h
@@ -11,8 +11,8 @@ namespace blunder {
 // Packages together a move with a prior probability from the evaluator and the visit count from the MCTS.
 struct MoveProb {
   Board board;
-  float prior;
-  unsigned visits;
+  float prior = 0.0;
+  unsigned visits = 0;
 };
 
 inline bool
@@ -22,6 +22,11 @@ operator<(const MoveProb& left, const MoveProb& right) noexcept
 // TODO: Don't want to have two copies of the board in best and moves, so
 // figure out a way to avoid the copy, e.g. have a pointer but make this into a
 // class.
+//
+// TODO: Include other search statics, e.g.,
+// - total number of nodes searched
+// - total time searching
+// - depth
 struct SearchResult {
   // The best move.
   MoveProb best;
