d614262a7939871db8a0da8a571567dcb147203d
Omar <oser520@gmail.com>
2023-09-05 22:48:11 -0400

    Remove inaccurate comment.

--- src/alpha_zero_decoder.h
+++ src/alpha_zero_decoder.h
@@ -7,9 +7,6 @@
 
 namespace blunder {
 
-// An interface for decoding the output Tensors from a neural network with value
-// and policy heads back into boards representing the results of the moves, and
-// the value of the position.
 class AlphaZeroDecoder : public TensorDecoder {
 public:
   // Converts |board_path| into a Tensor.
