7b7fbb39de8a4e479fc09b51d0b08addc385aed7
Omar <oser520@gmail.com>
2023-10-22 20:23:09 -0400

    Copy other search statistics in take_from.

--- src/search_result.cc
+++ src/search_result.cc
@@ -14,6 +14,11 @@ PlayResult::take_from(SearchResult result)
   presult.play_move = std::move(result.best);
   presult.value = result.value;
   presult.other_moves.reserve(result.moves.size());
+  presult.total_nodes_expanded = result.total_nodes_expanded;
+  presult.depth = result.depth;
+  presult.millis_per_eval = result.millis_per_eval;
+  presult.millis_eval = result.millis_eval;
+  presult.millis_search_time = result.millis_search_time;
   std::ranges::transform(
       result.moves,
       std::back_inserter(presult.other_moves),
