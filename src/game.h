dafcb3e2e430b98238c94bd0b82c4cd196905838
Omar <oser520@gmail.com>
2023-10-22 20:23:40 -0400

    Add statistics to game result.

--- src/game.h
+++ src/game.h
@@ -13,6 +13,24 @@ struct GameResult {
   Board game_start;
   std::vector<PlayResult> history;
   std::optional<Color> winner;
+
+  // Maximum nodes expanded in any one run.
+  std::uint64_t max_nodes_expanded = 0;
+
+  // The average number of nodes expanded per search.
+  std::uint64_t avg_nodes_expanded = 0;
+
+  // The maximum depth of a branch explored during search.
+  unsigned max_depth = 0;
+
+  // The average depth for each search run.
+  unsigned avg_depth = 0;
+
+  // Average number of milliseconds per evaluation of node for the entire game.
+  float millis_per_eval = 0;
+
+  // The average number of milliseconds per search for the entire game.
+  float millis_per_search = 0;
 };
 
 // An interface for a chess game.
