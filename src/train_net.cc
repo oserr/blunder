bf40479e4ff66ac8fd7ea092994643860475ec6b
Omar <oser520@gmail.com>
2023-12-19 22:00:56 -0500

    Figure out libtorch stuff.

--- src/train_net.cc
+++ src/train_net.cc
@@ -1,4 +1,7 @@
+#include <algorithm>
 #include <iostream>
+#include <format>
+#include <vector>
 
 #include "net.h"
 #include "torch/torch.h"
@@ -8,9 +11,34 @@ using namespace blunder;
 int
 main()
 {
-  AlphaZeroNet net;
+  c10::InferenceMode inference_mode;
+  for (int i = 0; i < 20; ++i)
+    std::cout << std::format("{:0>2}\n", i);;
 
-  torch::optim::SGD optimizer(net.parameters(), /*lr=*/0.02);
+  AlphaZeroNet net;
+  if (net.create_checkpoint("checkpoint")) {
+    std::cout << "checkpoint created successfully" << std::endl;
+  } else {
+    std::cout << "unable to create checkpoint" << std::endl;
+  }
+
+  AlphaZeroNet other_net;
+  if (other_net.load_checkpoint("checkpoint")) {
+    std::cout << "unable to load checkpoint" << std::endl;
+  } else {
+    std::cout << "checkpoint loaded successfully" << std::endl;
+  }
+
+  auto params = net.parameters();
+  for (auto& t : params) {
+    if (t.dim() >= 2) {
+      torch::nn::init::xavier_normal_(t);
+    } else {
+      torch::nn::init::zeros_(t);
+    }
+  }
+
+  torch::optim::SGD optimizer(params, /*lr=*/0.02);
   optimizer.zero_grad();
 
   // Dims are
@@ -18,7 +46,7 @@ main()
   // - number of channels (i.e. depth)
   // - number of columns
   // - rows
-  auto t = torch::zeros({1, 119, 8, 8});
+  auto t = torch::zeros({1, 119, 8, 8}).to(torch::kCUDA);
 
   std::cout << "Running net...." << std::endl;
 
@@ -26,9 +54,7 @@ main()
 
   std::cout << "Net finished running and...\n"
             << "v.dim() = " << v.dim()
-            << "v=" << v << " ...\n"
-            << "p.dim() = " << p.dim()
-            << "p=....\n" << p << std::endl;
+            << "\np.dim() = " << p.dim() << std::endl;
 
   return EXIT_SUCCESS;
 }
