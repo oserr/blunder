47315b340f4dc2bb308ce4075783d662954aa643
Omar <oser520@gmail.com>
2023-12-20 12:43:40 -0500

    Implement ChessDataSet to be used as a data loader.

---
+++ src/chess_data_set.cc
@@ -0,0 +1,61 @@
+#include "chess_data_set.h"
+
+#include <cstddef>
+#include <memory>
+#include <span>
+#include <stdexcept>
+#include <utility>
+
+#include "game_result.h"
+#include "tensor_encoder.h"
+
+#include <torch/torch.h>
+
+namespace blunder {
+
+ChessDataSet::ChessDataSet(
+    std::span<const GameResult> game_results,
+    std::shared_ptr<TensorEncoder> encoder)
+  : game_results(game_results),
+    encoder(std::move(encoder))
+{
+  if (not encoder)
+    throw std::invalid_argument("encoder cannot be null.");
+
+  for (const auto& gr : game_results)
+    num_examples += gr.moves.size();
+}
+
+ChessDataSet::ExampleType
+ChessDataSet::get(std::size_t index)
+{
+  if (index >= num_examples)
+    throw std::out_of_range("index is out of range");
+
+  const GameResult* game_result = nullptr;
+
+  for (const auto& gr : game_results) {
+    const auto num_moves = gr.moves.size();
+    if (num_moves > index) {
+      game_result = &gr;
+      break;
+    }
+    index -= num_moves;
+  }
+
+  if (not game_result)
+    throw std::runtime_error("Unable to find the game_result for index.");
+
+  auto input_tensor = encoder->encode_board(index == 0
+      ? game_result->game_start
+      : game_result->moves[index-1].best.board);
+
+  auto policy_tensor = encoder->encode_moves(game_result->moves[index].moves);
+
+  torch::Tensor value_tensor = torch::full({1}, game_result->moves[index].value);
+
+  return ExampleType(std::move(input_tensor),
+                     std::make_pair(policy_tensor, value_tensor));
+}
+
+} // namespace blunder
---
+++ src/chess_data_set.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cstddef>
+#include <memory>
+#include <span>
+#include <stdexcept>
+#include <utility>
+
+#include "game_result.h"
+#include "tensor_encoder.h"
+
+#include <torch/torch.h>
+
+namespace blunder {
+
+using TensorPair = std::pair<torch::Tensor, torch::Tensor>;
+
+// TODO: When we begin to generate training data through self play, we'll save
+// the game history, and this custom loader will need to read the data and
+// convert it to tensors.
+//
+// Placer holder code for a custom dataset to use with a dataloader.
+// Each example will consists of the input to the network, i.e. an 8x8x119 stack
+// of planes representing the current chess position, and the target will
+// consist of a pair of tensors in the form of (policy target, value target) to
+// represent the expected policy and value targets.
+class ChessDataSet :
+  public torch::data::datasets::Dataset<ChessDataSet, torch::data::Example<torch::Tensor, TensorPair>>
+{
+public:
+  ChessDataSet(
+      std::span<const GameResult> game_results,
+      std::shared_ptr<TensorEncoder> encoder);
+
+  //static constexpr bool is_stateful = false;
+
+  ExampleType
+  get(std::size_t index) override;
+
+  torch::optional<std::size_t>
+  size() const override
+  { return num_examples; };
+
+private:
+  // Note that this is not owned and hence the game results are expected to
+  // outlive ChessDataSet.
+  std::span<const GameResult> game_results;
+  std::shared_ptr<TensorEncoder> encoder;
+  std::size_t num_examples = 0;
+};
+
+} // namespace blunder
